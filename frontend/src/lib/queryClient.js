7b733ea8e8198334554f76f26548661616ad8271
Ian Myjer <imyjer@gmail.com>
2026-04-10 13:17:30 -0700

    polling

--- frontend/src/lib/queryClient.js
+++ frontend/src/lib/queryClient.js
@@ -3,7 +3,7 @@ import { QueryClient } from '@tanstack/svelte-query'
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 15_000,
+      staleTime: 5 * 60_000,
       refetchOnWindowFocus: true,
       retry: false,
     },
